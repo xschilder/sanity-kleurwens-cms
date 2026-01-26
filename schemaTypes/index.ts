8241454061196d392845400a931533c0710be084
xschilder <xschilder@proton.me>
2026-01-26 22:10:35 +0100

    Update index.ts
    Dla PRD v.1.5 i flow v.1.1 
    Dodanie 
    schemaTypes/quickDecisionMotivation.ts  (krok 5 przy małej kalkulacji}
    schemaTypes/guaranteeBlock.ts 
    (krok 5 przy dużej kalkulacji)

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,9 +2,14 @@
 import { beforeAfterProject } from './beforeAfterProject'
 import { testimonial } from './testimonial'
 import { project } from './project' // jeśli zostawiłeś poprzedni
+import { guaranteeBlock } from './guaranteeBlock';
+import { quickDecisionMotivation } from './quickDecisionMotivation';
+
 
 export const schemaTypes = [
   beforeAfterProject,
   testimonial,
   project, // możesz usunąć jeśli niepotrzebny
+  guaranteeBlock,              // Krok 5 - info odnośnie gwarancji przy wysokiej kalkulacji
+  quickDecisionMotivation,     // Krok 5 - info przy szybkiej decyzji na możliwy rabat
 ]
