9532c7078a52ebc29f96c4fb6fdb0c620b5d8e0a
xschilder <xschilder@proton.me>
2026-01-26 22:38:47 +0100

    Update index.ts

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -4,15 +4,12 @@ import { testimonial } from './testimonial'
 import { project } from './project' // jeśli zostawiłeś poprzedni
 import { guaranteeBlock } from './guaranteeBlock';
 import { quickDecisionMotivation } from './quickDecisionMotivation';
-import { defineType, defineField } from 'sanity'
 
 export const schemaTypes = [
   beforeAfterProject,
   testimonial,
   project, 
-  quickDecisionMotivation, // Krok 5 - info przy szybkiej decyzji na możliwy rabat
-  export const guaranteeBlock = defineType({ // Krok 5 - info odnośnie gwarancji przy wysokiej kalkulacji
-  name: 'guaranteeBlock',
-  title: 'Guarantee Block (≥ €1800)',
-  type: 'document',
+export { default as guaranteeBlock } from './guaranteeBlock.ts'
+export { default as quickDecisionMotivation } from './quickDecisionMotivation.ts'
+
 ]
